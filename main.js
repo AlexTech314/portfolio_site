7f7086b795479fc326c59f694f9df553a05fdd42
AlexTech314 <alex.s@wustl.edu>
2020-08-11 11:43:51 -0400

    Done with navbar for now

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $(`.menu-toggler`).on('click', function () {
+    $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
-        $(`.top-nav`).toggleClass('open');
+        $('.top-nav').toggleClass('open');
     })
 })

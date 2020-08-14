255e4d1687ddc3f6518fb481332b7cc6d51361d3
AlexTech314 <alex.s@wustl.edu>
2020-08-14 14:11:59 -0400

    adding personality section, backend for contact

--- main.js
+++ main.js
@@ -3,4 +3,22 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     })
-})
+
+    $('.top-nav .nav-link').on('click', function () {
+        $(`.menu-toggler`).removeClass('open');
+        $('.top-nav').removeClass('open');
+    })
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top -100
+        }, 2000);
+       
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+});
